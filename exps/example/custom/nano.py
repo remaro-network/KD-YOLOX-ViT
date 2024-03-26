59e2ae38558fa351422738c378c07711e821d9ba
martinaub <aubard.martin@gmail.com>
2024-03-26 17:09:01 +0000

    fix ViT for nano training

--- exps/example/custom/nano.py
+++ exps/example/custom/nano.py
@@ -85,7 +85,7 @@ class Exp(MyExp):
             from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
             in_channels = [256, 512, 1024]
             # NANO model use depthwise = True, which is main difference.
-            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, depthwise=True)
+            backbone = YOLOPAFPN(self.depth, self.width, in_channels=in_channels, depthwise=True, vit=self.vit)
             head = YOLOXHead(self.num_classes, self.width, in_channels=in_channels, depthwise=True)
             self.model = YOLOX(backbone, head)
 
